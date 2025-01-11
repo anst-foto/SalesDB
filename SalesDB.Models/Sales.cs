520c4a207e969f0ecc469ae96dd973f2137f51b8
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-11 13:22:27 +0300

    Добавление реализации IEquatable

--- SalesDB.Models/Sales.cs
+++ SalesDB.Models/Sales.cs
@@ -1,10 +1,34 @@
 ﻿namespace SalesDB.Models;
 
-public class Sales
+public class Sales : IEquatable<Sales>
 {
-    public long Id { get; set; }
-    public DateTime Date { get; set; }
-    public string ProductName { get; set; }
-    public int Amount { get; set; }
-    public decimal Price { get; set; }
+    public long Id { get; init; }
+    public DateTime Date { get; init; }
+    public string ProductName { get; init; }
+    public int Amount { get; init; }
+    public decimal Price { get; init; }
+
+    #region IEquatable<Sales>
+
+    public bool Equals(Sales? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id == other.Id && Date.Equals(other.Date) && ProductName == other.ProductName && Amount == other.Amount && Price == other.Price;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Sales)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Date, ProductName, Amount, Price);
+    }
+
+    #endregion
 }
