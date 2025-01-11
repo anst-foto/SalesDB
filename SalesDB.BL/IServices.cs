4d8ed1081c27700e578035a41252dfcb522ca570
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-11 12:23:43 +0300

    Добавление BL

---
+++ SalesDB.BL/IServices.cs
@@ -0,0 +1,10 @@
+﻿using SalesDB.Models;
+
+namespace SalesDB.BL;
+
+public interface IServices
+{
+    public IEnumerable<Sales> GetAllSales();
+    public bool AddSale(Sale sale);
+    public bool AddProduct(Product product);
+}
---
+++ SalesDB.BL/SalesServices.cs
@@ -0,0 +1,29 @@
+﻿using SalesDB.DAL;
+using SalesDB.Models;
+
+namespace SalesDB.BL;
+
+public class SalesServices : IServices
+{
+    private readonly ISalesRepository _db;
+
+    public SalesServices(ISalesRepository db)
+    {
+        _db = db;
+    }
+
+    public IEnumerable<Sales> GetAllSales()
+    {
+        return _db.GetAllSales();
+    }
+
+    public bool AddSale(Sale sale)
+    {
+        return _db.AddSale(sale);
+    }
+
+    public bool AddProduct(Product product)
+    {
+        return _db.AddProduct(product);
+    }
+}
